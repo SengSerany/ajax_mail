07cc8696a48860225c2a79f4a477091123cffe49
SengSerany <cranyseng@gmail.com>
2019-08-28 21:25:24 +0200

    add suppress ajax

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :tasks, except: [:show]
+  delete '/tasks/:id', to: 'tasks#destroy',  as: 'destroy_task'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
